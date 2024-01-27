e0e00e34ebf63620673a010ae0541862657b795f
Stormbreaker20 <luvsharma.del@gmail.com>
2024-01-27 12:14:42 +0530

    Artworks

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,61 @@ import Navbar from './assets/Navbar'
 import Home from './components/Home'
 import About from './components/About'
 import Artworks from './components/Artworks'
+import { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
 
 function App() {
-  
+  const [mousePosition, setMousePosition] = useState({
+    x: 0,
+    y: 0
+  });
+  const [cursorVariant, setCursorVariant] = useState("default");
+
+
+  useEffect(() => {
+    const mouseMove = e => {
+      setMousePosition({
+        x: e.clientX,
+        y: e.clientY
+      })
+    }
+
+    window.addEventListener("mousemove", mouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", mouseMove);
+    }
+  }, []);
+
+  const variants = {
+    default: {
+      x: mousePosition.x - 10,
+      y: mousePosition.y - 10,
+    },
+    text: {
+      height: 50,
+      width: 50,
+      x: mousePosition.x - 20,
+      y: mousePosition.y - 20,
+      backgroundColor: "white",
+      mixBlendMode: "difference"
+    }
+  }
+
+  const textEnter = () => setCursorVariant("text");
+  const textLeave = () => setCursorVariant("default");
   return (
     <>
       <Navbar />
+      <motion.div
+        className='cursor'
+        variants={variants}
+        animate={cursorVariant}
+      />
       <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/" element={<h1  onMouseEnter={textEnter} onMouseLeave={textLeave} className='title'>Home</h1>} />
         <Route path="/about" element={<h1>About</h1>} />
-        <Route path="/artworks" element={<h1>Artworks</h1>} />
+        <Route path="/artworks" element={<Artworks textEnter = {textEnter} textLeave = {textLeave} />} />
       </Routes>
     </>
   )
