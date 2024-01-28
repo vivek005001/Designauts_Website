ac4291a4c5f192f6e419bab8747df540f8be05d6
Stormbreaker20 <luvsharma.del@gmail.com>
2024-01-28 10:52:55 +0530

    Artworks Responsive - Nav updated

--- src/components/Artworks.jsx
+++ src/components/Artworks.jsx
@@ -42,13 +42,13 @@ const Artworks = ({textLeave,textEnter}) => {
   const titleSpring = (show) =>  useSpring({
     opacity: show ? 1 : 0,
     transform: show ? "scale(1)" : "scale(1.5)",
-    letterSpacing: show ? "0.1em" : "1.2rem",
+    letterSpacing: show ? "0.1em" : windowWidth < 800 ? windowWidth < 500 ? "0.2em" : "0.5em" : "0.8em",
   });
 
   const descSpring = (show) => useSpring({
     opacity: show ? 1 : 0,
     transform: show ? "scale(1) " : "scale(1.5)",
-    letterSpacing: show ? "0.1em" : "0.8em",
+    letterSpacing: show ? "0.1em" : windowWidth < 800 ? windowWidth < 500 ? "0.15em" : "0.3em"  : "0.8em",
   });
 
   const dateSpring = (show) => useSpring({
@@ -60,45 +60,47 @@ const Artworks = ({textLeave,textEnter}) => {
   const imageSpring = (end,extra) => useSpring({
     transform: `translate(-${(scrolled)/ 2}px, -${(scrolled)/ 2 }px)`,
   });
-
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   useEffect(() => {
     const handleScroll = () => {
       setScrolled(window.scrollY);
     };
-
     window.addEventListener("scroll", handleScroll);
+    window.addEventListener("resize", () => {
+      setWindowWidth(window.innerWidth);
+    });
 
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
   return (
-    <div className=" text-white items-center relative">
-      <div className="h-[1200vh] overflow-x-hidden">
-        <div onMouseEnter={textEnter} onMouseLeave={textLeave} className="flex justify-center items-center text-6xl h-[100vh] mb-[15rem]">
+    <div className=" text-white items-center ">
+      <div className=" overflow-hidden">
+        <div onMouseEnter={textEnter} onMouseLeave={textLeave} className="flex max-md:text-5xl max-sm:text-4xl justify-center items-center text-6xl h-[100vh] mb-[15rem]">
           Welcome to Artworks
         </div>
-        <div className="flex flex-col gap-[10rem]">
+        <div className="flex flex-col gap-[10rem] mx-auto relative max-xl:max-w-[80%] h-[400vh] max-md:h-[250vh] max-md:gap-5">
         {ArtWorksArr.map((artwork, index) => {
-          const show = (scrolled > (350 + index*500 - index*20)) && (scrolled < ((350 + index*450) + 300 + index*50));
-          const margin_val = (250*index).toString() + "px";
+          const show = (scrolled > (380 + index*500 + (windowWidth > 1500 ? - index*20 : windowWidth > 800 ? index*10 : (index + 1)*100 - 200*index ))) && (scrolled < ((380 + index*500) + (windowWidth > 1500 ? 300 : windowWidth > 800 ?  350 + 20*index : 400 - 100*index)));
+          const margin_val = ((windowWidth > 1900 ? 200 : (windowWidth < 1050 ? (windowWidth < 750 ? 200 : 300) : 300)  )*index).toString() + "px";
           return (
             <>
             <div key={index} className={`fixed z-30 top-[50vh] -rotate-90 left-0  overflow-visible`} >
             <animated.div style={dateSpring(show)} >
-              <span onMouseEnter={textEnter} onMouseLeave={textLeave} className="text-8xl">{artwork.date}</span>
+              <span onMouseEnter={textEnter} onMouseLeave={textLeave} className="text-8xl max-xl:text-6xl max-sm:text-4xl">{artwork.date}</span>
             </animated.div>
             </div>
             <div
             key={index}
               className={`fixed z-30 w-[100%] top-[40vh] left-24 overflow-hidden`}
             >
-              <Element name="scrollToElement">
+              <Element name="scrollToElement" class="max-sm:mr-20">
                 <animated.div className="text-center" style={titleSpring(show)}>
                   <h2 onMouseEnter={textEnter} onMouseLeave={textLeave} className="text-3xl">{artwork.title}</h2>
                 </animated.div>
                 <animated.div
-                  className="text-center text-base mt-5 "
+                  className="text-center  text-base mt-5 max-sm:text-sm  "
                   style={descSpring(show)}
                 >
                   <p onMouseEnter={textEnter} onMouseLeave={textLeave}>{artwork.desc}</p>
@@ -110,7 +112,7 @@ const Artworks = ({textLeave,textEnter}) => {
                 <img
                   src={artwork.img}
                   alt="" 
-                  className="h-[500px] w-[500px] rounded-lg brightness-75 z-0"
+                  className="h-[500px] w-[500px] max-lg:scale-[90%] max-xl:ml-[30rem] max-md:ml-[30rem] max-md:scale-75 rounded-lg brightness-75 z-0"
                 />
               </animated.div>
             </div>
