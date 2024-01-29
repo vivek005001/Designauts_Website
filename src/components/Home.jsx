fd67fccb1eda8757d09f283df180e6065e2eb14c
Stormbreaker20 <luvsharma.del@gmail.com>
2024-01-29 11:49:30 +0530

    Added Logo On Home Page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,8 +3,8 @@ import MainImg from '../images/Designauts_Main.png'
 
 const Home = () => {
   return (
-    <div className='text-white text-2xl pt-28 w-screen h-screen flex items-center justify-center'>
-      <img src={MainImg}></img>
+    <div className='text-white text-2xl pt-28 h-[100vh] bg-black flex items-center justify-center'>
+      <video src='/Website.mov' autoPlay muted />
     </div>
   )
 }
